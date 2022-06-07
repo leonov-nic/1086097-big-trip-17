7f8dec901d63b692c434bd8c1c5df63819fac6b2
Leonov-nick <leonov-nic@yandex.ru>
2022-06-07 23:31:32 +0300

    поправил баг в выводе общей цены

--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -3,26 +3,29 @@ import { createPeriodOfTrips }  from '../utils/trip';
 
 const createTripInfoTemplate = (trips) => {
   const period = trips.length ? createPeriodOfTrips(trips) : '';
-  const names = trips.map((item) => item.destination.name);
 
-  let basicСostOfAllOffers = trips.map((item) => item.basePrice).reduce((sum, elem) => sum + elem);
+
+  const namesOfDestination = trips.map((item) => item.destination.name);
+  const threeNamesOfDestination = trips.map((item) => item.destination.name).join(' — ');
+
+  const basicСostOfAllOffers = trips.map((item) => Number(item.basePrice)).reduce((sum, elem) => sum + elem);
   const costOfAllOffers = [];
   let totalСost = basicСostOfAllOffers;
   let sumOfAllOffers = 0;
-  trips.forEach(trip => {
+  trips.forEach((trip) => {
     if (trip.offers.offers) {
-      trip.offers.offers.forEach(offer => costOfAllOffers.push(offer.price));
+      trip.offers.offers.forEach((offer) => costOfAllOffers.push(offer.price));
     }
   });
   if (costOfAllOffers.length !== 0) {
     sumOfAllOffers = costOfAllOffers.reduce((sum, elem) => sum + elem);
     totalСost = sumOfAllOffers + basicСostOfAllOffers;
-  };
+  }
 
   return (`
     <section class="trip-main__trip-info  trip-info">
       <div class="trip-info__main">
-        <h1 class="trip-info__title">${names.slice(0, 1)} — ${names.slice(-1)}</h1>
+        <h1 class="trip-info__title">${trips.length > 3 ? `${namesOfDestination.slice(0, 1)} —...— ${namesOfDestination.slice(-1)}` : threeNamesOfDestination}</h1>
 
         <p class="trip-info__dates">${period}</p>
       </div>
