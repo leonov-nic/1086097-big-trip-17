da5c745181eb985dac5a9fb937e574ebd837de75
Leonov-nick <leonov-nic@yandex.ru>
2022-06-19 14:07:28 +0300

    поправил небольшой баг с типами

--- src/view/trip-form-view.js
+++ src/view/trip-form-view.js
@@ -63,7 +63,7 @@ const createTripFormTemplate = (trip, alloffers, alldestinations, newForm) => {
           <div class="event__type-wrapper">
             <label class="event__type  event__type-btn" for="event-type-toggle-1">
               <span class="visually-hidden">Choose event type</span>
-              <img class="event__type-icon" width="17" height="17" src="img/icons/${type ? type : '#'}.png" alt="${type ? `Event ${type} icon` : ''}">
+              <img class="event__type-icon" width="17" height="17" src="${type ? `img/icons/${type}.png` : ''}" alt="${type ? `Event ${type} icon` : ''}">
             </label>
             <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox"  ${isDisabled ? 'disabled' : ''}>
 
